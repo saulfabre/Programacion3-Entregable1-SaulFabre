8813b64e5a982b70762034aec9522d68c13ab9a8
saulfabre <saulfabre2@gmail.com>
2026-05-28 23:53:59 -0400

    Agregado clase GestionAcademica

---
+++ src/GestionAcademica.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class GestionAcademica {
+
+    private ArrayList <Estudiante> estudiantes;
+    private ArrayList <Profesor> profesores;
+    private ArrayList <Materia> materias;
+ 
+}
